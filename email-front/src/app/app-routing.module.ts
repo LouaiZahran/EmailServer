6f5370d8ecb47dcb7edefcafb3306128f278c051
AhmedRabea2100 <Ahmed.Rabea2100@gmail.com>
2021-12-27 20:47:09 +0200

    added log in and register, modified folders organization

--- email-front/src/app/app-routing.module.ts
+++ email-front/src/app/app-routing.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ComposeComponent } from './compose/compose.component';
-import { DraftComponent } from './draft/draft.component';
-import { FoldersComponent } from './folders/folders.component';
-import { InboxComponent } from './inbox/inbox.component';
-import { SentComponent } from './sent/sent.component';
-import { TrashComponent } from './trash/trash.component';
+import { ComposeComponent } from './mail/compose/compose.component';
+import { DraftComponent } from './mail/draft/draft.component';
+import { FoldersComponent } from './mail/folders/folders.component';
+import { InboxComponent } from './mail/inbox/inbox.component';
+import { SentComponent } from './mail/sent/sent.component';
+import { TrashComponent } from './mail/trash/trash.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MailComponent } from './mail/mail.component';
 
 const routes: Routes = [
-  {path: 'compose', component: ComposeComponent},
-  {path: 'inbox', component: InboxComponent},
-  {path: 'trash', component: TrashComponent},
-  {path: 'sent', component: SentComponent},
-  {path: 'draft', component: DraftComponent},
-  {path: 'folders', component: FoldersComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component:RegisterComponent},
+  {path: 'mail', component: MailComponent,
+   children: [
+    {path: 'compose', component: ComposeComponent}, 
+    {path: 'inbox', component: InboxComponent},
+    {path: 'trash', component: TrashComponent},
+    {path: 'sent', component: SentComponent},
+    {path: 'draft', component: DraftComponent},
+    {path: 'folders', component: FoldersComponent}
+  ]},
+  {path: '', redirectTo:'/login', pathMatch:'full'}
 ];
 
 @NgModule({
@@ -21,4 +30,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [ComposeComponent, InboxComponent, TrashComponent, SentComponent, DraftComponent, FoldersComponent];
+export const routingComponents =
+ [ComposeComponent, InboxComponent, TrashComponent, SentComponent, DraftComponent, FoldersComponent, LoginComponent, RegisterComponent, MailComponent];
